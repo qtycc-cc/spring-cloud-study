26e71075d6d3a48eb8f29c0c78750354a97e118c
Carmen <2942173241@qq.com>
2025-03-15 15:34:35 +0800

    配置中心demo

--- services/service-order/src/main/java/com/example/order/controller/OrderController.java
+++ services/service-order/src/main/java/com/example/order/controller/OrderController.java
@@ -6,17 +6,30 @@ import com.example.model.order.Order;
 import com.example.order.service.OrderService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@RefreshScope // 自动热刷新
 @RestController
 public class OrderController {
     @Autowired
     private OrderService orderService;
+    // 从配置中心读取
+    @Value("${order.timeout}")
+    String orderTimeout;
+    @Value("${order.auto-confirm}")
+    String orderAutoConfirm;
 
     @GetMapping("/create")
     public Order create(@RequestParam("productId") Long productId,
                         @RequestParam("userId") Long userId) {
         return orderService.createOrder(productId, userId);
+    }
+
+    @GetMapping("/config")
+    public String getConfi() {
+        return "orderTimeout: " + orderTimeout + ", " + "orderAutoConfirm: " + orderAutoConfirm;
     }
 }
