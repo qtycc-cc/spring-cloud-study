dfae3d3478d43ee568ba49ad27cb0f3bb5f12232
Carmen <2942173241@qq.com>
2025-03-15 20:35:34 +0800

    baseURL的设置=> 用于请求第三方api

--- services/service-order/src/main/java/com/example/order/service/OrderFeignService.java
+++ services/service-order/src/main/java/com/example/order/service/OrderFeignService.java
@@ -8,6 +8,7 @@ import com.example.model.product.Product;
 
 // 声明式请求
 // 自动负载均衡
+// 如果设置了url，表示的是baseURL
 @FeignClient("service-product")
 public interface OrderFeignService {
     // 发起 GET http://service-product/product/1
